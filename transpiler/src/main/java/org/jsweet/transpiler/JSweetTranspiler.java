66fa3c91444e64b0e5e9f3558ab310d0df9b43e4
Christian Kohlschütter <christian@kohlschutter.com>
2023-10-09 20:49:51 +0200

    Use system-wide temporary working directories, clean up after use
    Previously, jsweet would create temporary directories named ".jsweet" in
    the current working directory. These directories would not be cleaned up
    after use.
    
    Add a "configured directory" abstraction layer, which controls both
    user-provided and default-temporary directories, making sure the
    directories and its contents are deleted as soon as they're no longer
    needed, or at least upon VM shutdown.

--- transpiler/src/main/java/org/jsweet/transpiler/JSweetTranspiler.java
+++ transpiler/src/main/java/org/jsweet/transpiler/JSweetTranspiler.java
@@ -86,6 +86,7 @@ import com.google.debugging.sourcemap.SourceMapGeneratorFactory;
 import com.google.debugging.sourcemap.SourceMapGeneratorV3;
 import com.google.debugging.sourcemap.SourceMapping;
 import com.google.gson.Gson;
+
 import standalone.com.sun.source.tree.CompilationUnitTree;
 import standalone.com.sun.source.tree.Tree;
 
